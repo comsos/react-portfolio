82fe89b36a4bd6d6e0389d7344be9bed41acb950
cosmos <paulchristian946@gmail.com>
2023-07-04 13:43:36 +0800

    feat: added footer

--- src/App.tsx
+++ src/App.tsx
@@ -10,12 +10,12 @@ import AboutMe from './components/AboutMe';
 function App() {
   const [mode, setMode] = useState('light')
   return (
-    <>
+    <div className='flex-flex-col h-screen justify-between'>
       <Fade top distance='10%' duration={2000}>
           <BrowserRouter>
             <Header mode={mode} />
             <div className={`absolute w-screen h-screen z-[-10] ${mode == 'light' ? 'bg-[#e6e6e6]' : 'bg-black'}`}>
-              <button onClick={() => {mode == 'light' ? setMode('dark') : setMode('light')} } className={`font-nue absolute right-0 mr-12 text-${mode == 'light' ? 'black' : 'white'}`}>{ mode.charAt(0).toUpperCase() + mode.slice(1)}</button>
+              <button onClick={() => {mode == 'light' ? setMode('dark') : setMode('light')} } className={`font-nue absolute right-0 mr-12 ${mode == 'light' ? 'text-black' : 'text-white'} z-20`}>{ mode.charAt(0).toUpperCase() + mode.slice(1)}</button>
                 <Routes>
                   <Route path="/" element={ <HeroSection mode={mode}/> }/>
                   <Route path="/AboutMe" element={ <AboutMe mode={mode}/> }>
@@ -24,10 +24,15 @@ function App() {
                 </Routes>
               </div>
           </BrowserRouter>
-          <div className={`border-b-8 mb-32 w-screen  absolute h-20 bottom-0 ${mode == 'light' ? 'border-black' : 'border-white'}`} />
+          <div className={`border-b-8 mb-32 w-screen absolute h-20 bottom-0 ${mode == 'light' ? 'border-black' : 'border-white'}`}/>
+          <p className={`font-nue absolute inset-x-0 bottom-0 flex justify-center text-center ${mode == 'light' ? 'text-black' : 'text-white'}`}>
+            Designed and Created by: Cosmos Grimaldo <br/>
+            Technologies used: React, TailwindCSS <br/>
+            2023
+          </p>
       </Fade>
       
-    </>
+    </div>
   )
 }
 
--- src/components/TechStack.tsx
+++ src/components/TechStack.tsx
@@ -11,24 +11,17 @@ import React, { useState, } from 'react'
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
 import Fade from "react-reveal/Fade";
 
-export default function FrontEnd() {
-  return (
-    <div>TechStack</div>
-  )
-}
-
-
-
 export const TechStack = () => {
   return (
     <Fade left distance='10%'>
-        <div className="flex flex-col absolute space-y-2">
-             <div className="flex flex-row h-20 space-x-4">
-                <img src={html} alt="react" />
-                <img src={css} alt="react" />
-                <img src={tailwind} alt="react" />
-                <img src={javascript} alt="react" />
-                <img src={reactLogo} alt="react" />
+      
+        <div className="flex flex-col justify-center items-center h-screen">
+             <div className="flex flex-row space-x-4 mt-[-20rem]">
+                <img className='h-14' src={html} alt="react" />
+                <img className='h-14' src={css} alt="css" />
+                <img className='h-14' src={tailwind} alt="tailwind" />
+                <img className='h-14' src={javascript} alt="js" />
+                <img className='h-14' src={reactLogo} alt="react" />
             </div> 
             <div className="flex flex-row h-20 space-x-4">
                 <img src={reactLogo} alt="react" />
