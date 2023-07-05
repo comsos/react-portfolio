ad86c30cdb5d671906860e65523e964e50d90d8c
cosmos <paulchristian946@gmail.com>
2023-07-05 13:08:49 +0800

    fix: renu link

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -18,7 +18,7 @@ export default function Projects({mode}: ProjectsProps) {
                 <img src={renu} alt="renu" className='h-48' />
                 <div className="flex flex-row">
                     <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path fill="currentColor" d="M6 7c0 .55.45 1 1 1h7.59l-8.88 8.88a.996.996 0 1 0 1.41 1.41L16 9.41V17c0 .55.45 1 1 1s1-.45 1-1V7c0-.55-.45-1-1-1H7c-.55 0-1 .45-1 1z"/></svg>
-                    <a href='renunaturals.com' target='_blank' className={`text-center font-nue text-nav ${mode == 'light' ? 'text-black' : 'text-white'}`}>Renu Naturals</a>
+                    <a href='https://renunaturals.com/' target='_blank' className={`text-center font-nue text-nav ${mode == 'light' ? 'text-black' : 'text-white'}`}>Renu Naturals</a>
                 </div>
                 
             </div>
