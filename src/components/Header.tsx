ce85f5582c265055ed478b4a43953c347d69bb1e
cosmos <paulchristian946@gmail.com>
2024-11-16 14:15:22 +0800

    chore: removed practicum and added a placeholder for mobile view TODO

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,60 +1,61 @@
-import { useEffect} from 'react'
+import { useEffect } from 'react'
 import Typewriter from "typewriter-effect";
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom"
 import '../App.css'
 
 interface HeaderProps {
-  mode: string;
+    mode: string;
 }
 
-export default function Header({mode}: HeaderProps) {
-  useEffect(() => {
-    // This block of code will run whenever the `count` prop changes
-    console.log('Count prop changed:', mode);
-  }, [mode]);
-  return (
-    <div className={`font-nue mt-10 ml-10 absolute ${mode == 'light' ? 'text-black' : 'text-white'} `}>
-        <h1 className={`text-name md:text-5xl`}  >Cosmos Grimaldo</h1>
-        <div className={`text-3xl`}>
-          <Typewriter
-              options={{
-                  loop: true,
-              }}
-              onInit={(typewriter) => {
-                  typewriter
-                      .typeString("Shopify")
-                      .pauseFor(1000)
-                      .deleteAll()
-                      .typeString("ERPNext")
-                      .pauseFor(1000)
-                      .deleteAll()
-                      .typeString("Full-Stack")
-                      .pauseFor(1000)
-                      .start();
-              }}
-            />
+export default function Header({ mode }: HeaderProps) {
+    useEffect(() => {
+    }, [mode]);
+    return (
+        <div className={`font-nue absolute ${mode == 'light' ? 'text-black' : 'text-white'} `}>
+            <div className='h-screen w-screen flex items-center sm:hidden bg-black text-white text-5xl'>
+                <div className='text-center '> This Portfolio is currently not available on mobile view. </div>
+            </div>
+            <div className='mt-10 ml-10'>
+                <h1 className={`text-name md:text-5xl`}  >Cosmos</h1>
+                <div className={`text-3xl`}>
+                    <Typewriter
+                        options={{
+                            loop: true,
+                        }}
+                        onInit={(typewriter) => {
+                            typewriter
+                                .typeString("Shopify")
+                                .pauseFor(1000)
+                                .deleteAll()
+                                .typeString("ERPNext")
+                                .pauseFor(1000)
+                                .deleteAll()
+                                .typeString("Full-Stack")
+                                .pauseFor(1000)
+                                .start();
+                        }}
+                    />
+                </div>
+                <ul className='pt-24 text-nav'>
+                    <li key='TechStack'>
+                        <Link to={"/"}>Home</Link>
+                    </li>
+                    <li key='AboutMe'>
+                        <Link to={"/AboutMe"}>About Me</Link>
+                    </li>
+                    <li key='TechStack'>
+                        <Link to={"/TechStack"}>Tech Stack</Link>
+                    </li>
+                    <li key='Projects'>
+                        <Link to={"/Projects"}>Projects</Link>
+                    </li>
+                    <li key='Links'>
+                        <Link to={"/Links"}>Links</Link>
+                    </li>
+                </ul>
+            </div>
+
+
         </div>
-      <ul className='pt-24 text-nav'>
-          <li key='TechStack'>
-            <Link to={"/"}>Home</Link>
-          </li>
-          <li key='AboutMe'>
-            <Link to={"/AboutMe"}>About Me</Link>
-          </li>
-          <li key='TechStack'>
-            <Link to={"/TechStack"}>Tech Stack</Link>
-          </li>
-          <li key='Projects'>
-            <Link to={"/Projects"}>Projects</Link>
-          </li>
-          <li key='Links'>
-            <Link to={"/Practicum"}>Practicum</Link>
-          </li>
-          <li key='Links'>
-            <Link to={"/Links"}>Links</Link>
-          </li>     
-      </ul>
-      
-    </div>
-  )
+    )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -36,18 +36,26 @@ export default {
       '7xl': '5rem',
     },
     screens: {
-      xs: '375px',
-      sm: '640px',
-      md: '768px',
-      lg: '1024px',
-      xl: '1280px',
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
       '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
     },
     container: {
       center: true,
       padding: "1rem",
     },
-    fontFamily:{
+    fontFamily: {
       'nue': ['Neue Montreal'],
     }
   },
